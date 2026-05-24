2065885ed0bd304fa245bfd4048451f3869bc6b7
Nathaniel Cowperthwaite <masternate1996@gmail.com>
2026-05-24 16:45:40 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,8 +24,8 @@ export default function Home() {
         <PullToRefresh onRefresh={handleRefresh}>
           <Hero />
           <FeaturedMenu />
-          <About />
           <VideoShowcase />
+          <About />
           <TruckRequest />
           <CateringTeaser />
           <LoyaltyCTA />
